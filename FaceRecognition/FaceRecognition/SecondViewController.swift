1a95f3425739e803c7a8093c9087d3037b607d56
DeveloperMetehan <113914596+DeveloperMetehan@users.noreply.github.com>
2022-09-20 20:55:01 +0300

    CreateFaceRecognizer
    Face Recognizer is created

---
+++ FaceRecognition/FaceRecognition/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  FaceRecognition
+//
+//  Created by Gappze on 21.07.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
